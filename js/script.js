ce87687f7e14b827a57dcdb8582ea7db00a668e7
takayamayoshihisa1013 <takayama.yoshihisa1013@gmail.com>
2025-09-09 19:23:10 +0900

    下向きに大きく振ったときにおみくじが出るようにする

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@ function handleMotion(event) {
     // 合成加速度を表示
     magnitude_result.textContent = magnitude;
 
-    if (magnitude > 20) {
+    if (magnitude > 20 && (y > 15 || y < -15)) {
       const fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
       omikuji_result.textContent = fortune;
     }
