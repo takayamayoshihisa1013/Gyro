29f48b3e8ba7339fd1ec1332ba806c50b3d613ed
takayamayoshihisa1013 <takayama.yoshihisa1013@gmail.com>
2025-09-10 15:35:55 +0900

    増やしすぎた

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@ function handleMotion(event) {
     // 合成加速度を表示
     magnitude_result.textContent = magnitude;
 
-    if (y > 60 || y < -60) {
+    if (y > 50 || y < -50) {
       const fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
       omikuji_result.textContent = fortune;
     }
